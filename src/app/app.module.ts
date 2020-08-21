ad1635951d59c35f68f1c9a63af97906e65912d5
ealfonsov89 <ealfonsov89@gmail.com>
2020-08-21 13:45:33 +0200

    add api connection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, LOCALE_ID  } from '@angular/core';
-import { AppComponent } from './app.component';
-import { DateModule } from './atoms/date/date.module';
-import {HttpClient, HttpClientModule} from '@angular/common/http';
-import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
-import {TranslateHttpLoader} from '@ngx-translate/http-loader';
-import { registerLocaleData } from '@angular/common';
-import localeFr from '@angular/common/locales/fr';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, LOCALE_ID } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { DateModule } from "./atoms/date/date.module";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { TranslateModule, TranslateLoader } from "@ngx-translate/core";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+import { registerLocaleData } from "@angular/common";
+import localeFr from "@angular/common/locales/fr";
+import { Observable } from "rxjs";
 
-// AoT requires an exported function for factories
-export function HttpLoaderFactory(httpClient: HttpClient) {
-  return new TranslateHttpLoader(httpClient);
+export class CustomLoader implements TranslateLoader {
+  constructor(private http: HttpClient) {}
+
+  public getTranslation(iso_lang: String): Observable<any> {
+    const lang = iso_lang.replace("_", "/");
+    // in this case can be any url
+    return this.http.get(`assets/i18n/${lang}.json`);
+  }
 }
-registerLocaleData(localeFr, 'fr');
+registerLocaleData(localeFr, "fr");
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     DateModule,
     BrowserModule,
@@ -24,7 +28,7 @@ registerLocaleData(localeFr, 'fr');
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
+        useClass: CustomLoader,
         deps: [HttpClient]
       }
     })
@@ -32,4 +36,4 @@ registerLocaleData(localeFr, 'fr');
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
